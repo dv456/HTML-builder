ba5661746fcc297077b6aaada00e590415dffc19
dv456 <72670501+dv456@users.noreply.github.com>
2022-05-23 16:14:56 +0300

    fix: fixed bugs

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -7,9 +7,9 @@ async function fileRead() {
         const files = await readdir(filePath,{encoding: 'utf-8', withFileTypes: true});
         for (const file of files){
             if (file.isFile()) {               
-                const way =path.join(__dirname, 'secret-folder',file.name);    
-                const stats = await fs.stat(way);   
-                console.log( `${file.name.split('.')[0]}  ${file.name.split('.')[1]} - ${stats.size/1000}kb\n`);                
+                const newfilePath =path.join(__dirname, 'secret-folder',file.name);    
+                const info = await fs.stat(newfilePath);   
+                console.log( `${file.name.split('.')[0]}  ${file.name.split('.')[1]} - ${info.size/1000}kb\n`);                
             }
         }
         } catch (err) {
